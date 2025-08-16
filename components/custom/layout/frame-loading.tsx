6212867382783b966aaab4fce30dfb8f0c44f410
Okonutnut <darlitocabalse.acad@gmail.com>
2025-08-16 16:16:47 +0800

    refactor: Wrap SkeletonLoading in a section for consistent layout

--- components/custom/layout/frame-loading.tsx
+++ components/custom/layout/frame-loading.tsx
@@ -1,5 +1,9 @@
 import SkeletonLoading from "@/components/custom/layout/skeleton-loading";
 
 export default function FrameLoading() {
-  return <SkeletonLoading />;
+  return (
+    <section className="w-screen h-screen p-4">
+      <SkeletonLoading />
+    </section>
+  );
 }
