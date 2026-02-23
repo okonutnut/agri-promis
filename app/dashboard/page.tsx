ca9865fa202fb45959a1f7400ac222bc4d669a81
Okonutnut <darlitocabalse.acad@gmail.com>
2026-02-23 20:25:00 +0800

    Add DashboardClient component with tabbed navigation for admin dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,36 +1,5 @@
-import AdminDashboardItems from "@/components/custom/dashboard/admin/admin-dashboard-items";
-import CustomPageLayout from "@/components/custom/layout/custom-page-layout";
-import { getDashboardNavItems } from "@/components/sidebar/navitems";
-import { format } from "date-fns/format";
-import { CustomTabList } from "@/components/custom/layout/custom-tab-list";
-import TravelOrdersAnalytics from "@/components/custom/dashboard/admin/travel-orders-analytics";
-import FCAAnalytics from "@/components/custom/dashboard/admin/fca-analytics";
+import DashboardClient from "@/app/dashboard/components/dashboardClient";
 
 export default function DashboardPage() {
-  const now = new Date();
-
-  return (
-    <CustomPageLayout
-      pageTitle="Dashboard"
-      pageDescription={`As of ${format(now, "PPpp")}`}
-      navItems={getDashboardNavItems()}
-    >
-      <CustomTabList
-        tabs={[
-          {
-            title: "Overview",
-            content: <AdminDashboardItems />,
-          },
-          {
-            title: "Travel Orders",
-            content: <TravelOrdersAnalytics />,
-          },
-          {
-            title: "FCA",
-            content: <FCAAnalytics />,
-          },
-        ]}
-      />
-    </CustomPageLayout>
-  );
+  return <DashboardClient />;
 }
